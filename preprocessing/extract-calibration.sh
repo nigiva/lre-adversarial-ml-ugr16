50cc7785eeb85c6407e6ff7a62cf6c9ed9eef662
Corentin Duchêne <corentin.duchene@epita.fr>
2022-04-19 15:57:10 +0200

    [ADD] calibration & test extract shell script

---
+++ preprocessing/extract-calibration.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+# Extract label from calibration dataset
+#
+#$ -S /bin/bash
+#$ -N MLSECU_extract_calibration
+#$ -wd ~/
+#$ -j y
+#$ -pe mpi 8
+#$ -o /media/silver/corentin/mlsecu/std
+#$ -t 1-17
+echo "Task id : $SGE_TASK_ID"
+declare -i id=$SGE_TASK_ID-1
+echo "Id : $id"
+
+month_list=(
+	march
+	march
+	march
+	april
+	april
+	april
+	april
+	may
+	may
+	may
+	may
+	may
+	may
+	june
+	june
+	june
+	june
+)
+
+week_list=(
+	week3
+	week4
+	week5
+	week2
+	week3
+	week4
+	week5
+	week1
+	week2
+	week3
+	week4
+	week5
+	week6
+	week1
+	week2
+	week3
+	week4
+)
+
+base_link_list="https://nesg.ugr.es/nesg-ugr16/download/normal"
+
+# Creer le dossier temporaire
+mkdir /tmp/mlsecu
+
+week=${week_list[$id]}
+month=${month_list[$id]}
+url="$base_link_list/$month/$week/$month"_"$week"_"csv.tar.gz"
+
+wget -O /tmp/mlsecu/mlsecu_data.tar.gz $url
+pv /tmp/mlsecu/mlsecu_data.tar.gz | tar xz -C /tmp/mlsecu
+
+dataset_name="calibration-$month.$week"
+data_path="/tmp/mlsecu/uniq/$month.$week.csv.uniqblacklistremoved"
+export_path="/media/silver/corentin/mlsecu/extract/calibration-$month.$week.json"
+index_path="/media/gold/corentin/mlsecu/dataset_label_index.json"
+
+/media/gold/corentin/mlsecu/venv/bin/python /media/gold/corentin/mlsecu/extract.py --data $data_path --index $index_path --dataset_name $dataset_name --export $export_path
+
+rm -rf /tmp/mlsecu
